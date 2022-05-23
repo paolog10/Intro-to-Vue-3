7160e5622e03fe0588f63fc6f7e050889c4888c1
Paolo Gamarra <paologamarra10@gmail.com>
2022-05-23 16:18:27 -0300

    lección 4 arreglo de la solución

--- main.js
+++ main.js
@@ -10,7 +10,15 @@ const app = Vue.createApp({
                 { id: 2235, color: 'blue' },
             ],
 
-            sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+            sizes: [
+                {index: 0, size: 'XS'},
+                {index: 1, size: 'S'},
+                {index: 2, size: 'M'},
+                {index: 3, size: 'L'},
+                {index: 4, size: 'XL'},
+                {index: 5, size: 'XXL'},
+                {index: 6, size: 'XXXL'}
+            ]
         }
     }
 })
