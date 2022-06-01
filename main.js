dce716401b21f8463155c943072fb14c0da8f6a2
Paolo Gamarra <paologamarra10@gmail.com>
2022-06-01 15:56:07 -0300

    Lección 8 corregida

--- main.js
+++ main.js
@@ -3,22 +3,46 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery', //computed property
+            //image: './assets/images/socks_blue.jpg',
+            selectedVariant:0,
+            //inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' ,quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            onSale: true
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+
+    //computed properties
+    computed:{ 
+        title(){
+            return this.brand + ' ' + this.product;
+        },
+
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+
+        sale(){
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.';
+            }
+            return '';
         }
     }
 })
